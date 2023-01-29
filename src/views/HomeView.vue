af4b13a5632d5064de70393592d388e07dff7551
lyae <lyaesley@gmail.com>
2023-01-29 23:04:22 +0900

    test commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div> 로스트아크 경매장 최적검색 시작
+    </div>
   </div>
 </template>
 
